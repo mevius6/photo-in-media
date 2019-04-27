7229179e3fe1e376a2742f38e3cc9c802607b6da
Nicolai <iplexus@icloud.com>
2019-04-27 07:20:58 +0300

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,58 @@
-if (screen && screen.width > 480) {
-    /**
-     * Called when there was a cables error.
-     * @param {string} errId - ID of the error, e.g. 'NO_WEBGL' or 'NO_WEBAUDIO'
-     * when the browser does not support the used APIs
-     * @param {string} errMsg - The error message
-     */
-    function showError(errId, errMsg) {
-        alert("An error occured: " + errId + ", " + errMsg);
-    }
+var main = document.querySelector("main");
 
-    function patchInitialized() {
-        // You can now access the patch object (CABLES.patch), register variable watchers and so on
-    }
+var mql = window.matchMedia("(max-width: 600px)");
 
-    function patchFinishedLoading() {
-        // The patch is ready now, all assets have been loaded
-        setTimeout(function() {
-            $(".app__loader").fadeOut("slow", function() {
-                $(".app__loader-wrap").fadeOut();
-            });
-        }, 1000);
-    }
-
-    document.addEventListener("DOMContentLoaded", function(event) {
-        CABLES.patch = new CABLES.Patch({
-            patchFile: "js/app.json",
-            prefixAssetPath: "",
-            glCanvasId: "glcanvas",
-            glCanvasResizeToWindow: true,
-            onError: showError,
-            onPatchLoaded: patchInitialized,
-            onFinishedLoading: patchFinishedLoading
+function screenTest(e) {
+    if (e.matches) {
+        $(window).on("load", function(event) {
+            setTimeout(function() {
+                $(".app__loader-wrap").fadeOut("slow", function() {
+                    $("main").removeClass("hidden");
+                });
+            }, 1000);
         });
-    });
-} else {
-    $(window).on("load", function(event) {
-        setTimeout(function() {
-            $(".app__loader-wrap").fadeOut("slow", function() {
-                $("main").removeClass("hidden");
+        $("canvas").hide();
+    } else {
+        /**
+         * Called when there was a cables error.
+         * @param {string} errId - ID of the error, e.g. 'NO_WEBGL' or 'NO_WEBAUDIO'
+         * when the browser does not support the used APIs
+         * @param {string} errMsg - The error message
+         */
+        function showError(errId, errMsg) {
+            alert("An error occured: " + errId + ", " + errMsg);
+        }
+
+        function patchInitialized() {
+            // You can now access the patch object (CABLES.patch), register variable watchers and so on
+        }
+
+        function patchFinishedLoading() {
+            // The patch is ready now, all assets have been loaded
+            setTimeout(function() {
+                $(".app__loader").fadeOut("slow", function() {
+                    $(".app__loader-wrap").fadeOut();
+                });
+            }, 1000);
+        }
+
+        document.addEventListener("DOMContentLoaded", function(event) {
+            CABLES.patch = new CABLES.Patch({
+                patchFile: "js/app.json",
+                prefixAssetPath: "",
+                glCanvasId: "glcanvas",
+                glCanvasResizeToWindow: true,
+                onError: showError,
+                onPatchLoaded: patchInitialized,
+                onFinishedLoading: patchFinishedLoading
             });
-        }, 1000);
-    });
-    $("canvas").hide();
+        });
+    }
 }
+
+screenTest(mql);
+mql.addListener(screenTest);
+
+mql.onchange = function() {
+    console.log(mql);
+};
