98cc5f4cde865e32747583f61fc7f2dca97d9654
Nicolai <iplexus@icloud.com>
2019-05-13 18:49:43 +0300

    add native lazy loading

--- js/app.js
+++ js/app.js
@@ -53,6 +53,27 @@ if (supportsCssVars()) {
                     onFinishedLoading: patchFinishedLoading
                 });
             });
+
+            (function() {
+                if ("loading" in HTMLImageElement.prototype) {
+                    var lazyEls = document.querySelectorAll("[loading=lazy]");
+                    lazyEls.forEach(function(lazyEl) {
+                        lazyEl.setAttribute(
+                            "src",
+                            lazyEl.getAttribute("data-src")
+                        );
+                    });
+                } else {
+                    var script = document.createElement("script");
+                    script.async = true;
+                    script.src =
+                        "https://cdn.jsdelivr.net/npm/vanilla-lazyload@12.0.0/dist/lazyload.min.js";
+                    window.lazyLoadOptions = {
+                        elements_selector: "[loading=lazy]"
+                    };
+                    document.body.appendChild(script);
+                }
+            })();
         }
     }
 
