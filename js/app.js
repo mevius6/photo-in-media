c9be0bbbbc1342a50e487d9ea4f6c0edc43837fd
Nicolai <iplexus@icloud.com>
2019-05-15 00:16:21 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,69 +1,69 @@
 var supportsCssVars = function() {
-    return window.CSS && CSS.supports("font-size", "var(--fs-bs)");
-};
+	return window.CSS && CSS.supports("font-size", "var(--fs-bs)")
+}
 
 var fadeOutLoader = function() {
-    $(".app__loader").fadeOut("slow", function() {
-        $(".app__loader-wrap").fadeOut();
-    });
-};
+	$(".app__loader").fadeOut("slow", function() {
+		$(".app__loader-wrap").fadeOut()
+	})
+}
 
-var canvas = document.getElementById("glcanvas");
+var canvas = document.getElementById("glcanvas")
 
-var main = document.querySelector("main");
+var main = document.querySelector("main")
 
-var mql = window.matchMedia("(max-width: 600px)");
+var mql = window.matchMedia("(max-width: 600px)")
 
 if (supportsCssVars()) {
-    function deviceWidth(e) {
-        if (e.matches) {
-            window.onload = function() {
-                setTimeout(fadeOutLoader, 1000);
-            };
-            canvas.classList.add("hidden");
-            main.classList.remove("hidden");
-        } else {
-            /**
-             * Called when there was a cables error.
-             * @param {string} errId - ID of the error, e.g. 'NO_WEBGL' or 'NO_WEBAUDIO'
-             * when the browser does not support the used APIs
-             * @param {string} errMsg - The error message
-             */
-            function showError(errId, errMsg) {
-                alert("An error occured: " + errId + ", " + errMsg);
-            }
+	function deviceWidth(e) {
+		if (e.matches) {
+			window.onload = function() {
+				setTimeout(fadeOutLoader, 1000)
+			}
+			canvas.classList.add("hidden")
+			main.classList.remove("hidden")
+		} else {
+			/**
+			 * Called when there was a cables error.
+			 * @param {string} errId - ID of the error, e.g. 'NO_WEBGL' or 'NO_WEBAUDIO'
+			 * when the browser does not support the used APIs
+			 * @param {string} errMsg - The error message
+			 */
+			function showError(errId, errMsg) {
+				alert("An error occured: " + errId + ", " + errMsg)
+			}
 
-            function patchInitialized() {
-                // You can now access the patch object (CABLES.patch), register variable watchers and so on
-            }
+			function patchInitialized() {
+				// You can now access the patch object (CABLES.patch), register variable watchers and so on
+			}
 
-            function patchFinishedLoading() {
-                // The patch is ready now, all assets have been loaded
-                setTimeout(fadeOutLoader, 1000);
-            }
+			function patchFinishedLoading() {
+				// The patch is ready now, all assets have been loaded
+				setTimeout(fadeOutLoader, 1000)
+			}
 
-            document.addEventListener("DOMContentLoaded", function(event) {
-                CABLES.patch = new CABLES.Patch({
-                    patchFile: "js/app.json",
-                    prefixAssetPath: "",
-                    glCanvasId: "glcanvas",
-                    glCanvasResizeToWindow: true,
-                    onError: showError,
-                    onPatchLoaded: patchInitialized,
-                    onFinishedLoading: patchFinishedLoading
-                });
-            });
-        }
-    }
+			document.addEventListener("DOMContentLoaded", function(event) {
+				CABLES.patch = new CABLES.Patch({
+					patchFile: "js/app.json",
+					prefixAssetPath: "",
+					glCanvasId: "glcanvas",
+					glCanvasResizeToWindow: true,
+					onError: showError,
+					onPatchLoaded: patchInitialized,
+					onFinishedLoading: patchFinishedLoading
+				})
+			})
+		}
+	}
 
-    deviceWidth(mql);
-    mql.addListener(deviceWidth);
+	deviceWidth(mql)
+	mql.addListener(deviceWidth)
 
-    mql.onchange = function() {
-        console.log(mql);
-    };
+	mql.onchange = function() {
+		console.log(mql)
+	}
 } else {
-    alert(
-        "Ваш браузер устарел, пожалуйста, используйте более современный браузер чтобы улучшить свои впечатления и безопасность!"
-    );
+	alert(
+		"Ваш браузер устарел, пожалуйста, используйте более современный браузер чтобы улучшить свои впечатления и безопасность!"
+	)
 }
