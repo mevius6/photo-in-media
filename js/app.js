622ef3dc8a990c997a5b98947411e5fa388578e1
Nicolai <iplexus@icloud.com>
2019-08-16 05:52:39 +0300

    rm jQuery

--- js/app.js
+++ js/app.js
@@ -1,67 +1,63 @@
-var supportsCssVars = function() {
-	return window.CSS && CSS.supports("font-size", "var(--fs-bs)")
-}
+{
+	const supportsCssVars = function () {
+		return window.CSS && CSS.supports("font-size", "var(--fs-bs)")
+	}
 
-var fadeOutLoader = function() {
-	$(".app__loader").fadeOut("slow", function() {
-		$(".app__loader-wrap").fadeOut()
-	})
-}
+	const canvas = document.getElementById("glcanvas")
 
-var canvas = document.getElementById("glcanvas")
+	const main = document.querySelector("main")
 
-var main = document.querySelector("main")
+	const mql = window.matchMedia("(max-width: 600px)")
 
-var mql = window.matchMedia("(max-width: 600px)")
+	if (supportsCssVars()) {
+		function deviceWidth(e) {
+			if (e.matches) {
+				window.onload = function () {
+					document.body.classList.remove('loading')
+				}
+				canvas.classList.add("hidden");
+				main.classList.remove("hidden")
+			} else {
+				/**
+				 * Called when there was a cables error.
+				 * @param {string} errId - ID of the error, e.g. 'NO_WEBGL' or 'NO_WEBAUDIO'
+				 * when the browser does not support the used APIs
+				 * @param {string} errMsg - The error message
+				 */
+				function showError(errId, errMsg) {
+					alert("An error occured: " + errId + ", " + errMsg)
+				}
 
-if (supportsCssVars()) {
-	function deviceWidth(e) {
-		if (e.matches) {
-			window.onload = function() {
-				setTimeout(fadeOutLoader, 1000)
-			}
-			canvas.classList.add("hidden")
-			main.classList.remove("hidden")
-		} else {
-			/**
-			 * Called when there was a cables error.
-			 * @param {string} errId - ID of the error, e.g. 'NO_WEBGL' or 'NO_WEBAUDIO'
-			 * when the browser does not support the used APIs
-			 * @param {string} errMsg - The error message
-			 */
-			function showError(errId, errMsg) {
-				alert("An error occured: " + errId + ", " + errMsg)
-			}
+				function patchInitialized() {
+					// You can now access the patch object (CABLES.patch), register variable watchers and so on
+				}
 
-			function patchInitialized() {
-				// You can now access the patch object (CABLES.patch), register variable watchers and so on
-			}
+				function patchFinishedLoading() {
+					// The patch is ready now, all assets have been loaded
+					document.body.classList.remove('loading')
+				}
 
-			function patchFinishedLoading() {
-				// The patch is ready now, all assets have been loaded
-				setTimeout(fadeOutLoader, 1000)
-			}
-
-			document.addEventListener("DOMContentLoaded", function(event) {
-				CABLES.patch = new CABLES.Patch({
-					patch: CABLES.exportedPatch,
-					prefixAssetPath: "img",
-					glCanvasId: "glcanvas",
-					glCanvasResizeToWindow: true,
-					onError: showError,
-					onPatchLoaded: patchInitialized,
-					onFinishedLoading: patchFinishedLoading
+				document.addEventListener("DOMContentLoaded", function (event) {
+					CABLES.patch = new CABLES.Patch({
+						patch: CABLES.exportedPatch,
+						prefixAssetPath: "img",
+						glCanvasId: "glcanvas",
+						glCanvasResizeToWindow: true,
+						onError: showError,
+						onPatchLoaded: patchInitialized,
+						onFinishedLoading: patchFinishedLoading
+					})
 				})
-			})
+			}
 		}
-	}
 
-	deviceWidth(mql)
-	mql.addListener(deviceWidth)
+		deviceWidth(mql)
+		mql.addListener(deviceWidth)
 
-	mql.onchange = function() {
-		console.log(mql)
+		mql.onchange = function () {
+			console.log(mql)
+		}
+	} else {
+		alert("Пожалуйста, просмотрите этот вебсайт в современном браузере, который поддерживает переменные CSS.")
 	}
-} else {
-	alert("Пожалуйста, обновите свой браузер чтобы улучшить впечатления и безопасность!")
 }
